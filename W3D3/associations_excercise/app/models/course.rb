d7d7d697898ec72fd80d224d37524d8415f19c34
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2016-11-02 12:05:42 -0400

    Complete Associations exercise

---
+++ W3D3/associations_excercise/app/models/course.rb
@@ -0,0 +1,25 @@
+# == Schema Information
+#
+# Table name: courses
+#
+#  id            :integer          not null, primary key
+#  name          :string(255)
+#  prereq_id     :integer
+#  instructor_id :integer
+#  created_at    :datetime
+#  updated_at    :datetime
+#
+
+class Course < ActiveRecord::Base
+
+  belongs_to :prereq,
+    class_name: 'Course',
+    primary_key: :id,
+    foreign_key: :prereq_id
+
+  belongs_to :instructor,
+    class_name: 'User',
+    primary_key: :id,
+    foreign_key: :instructor_id
+    
+end
